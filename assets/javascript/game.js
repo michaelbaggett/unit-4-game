15b315e1235540e2a9c5bad00992b9c0abebd84a
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-10-30 18:36:39 -0500

    added more pseudo code to being coding js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,3 +8,17 @@
     //on click, amount of crystal is added to our score
 //row that lists our current score
 
+
+var score = 0
+
+//computer generates random number the user must try to match
+
+//crystal random number generated
+
+//hide crystal amount from user
+
+//click functions for crystals
+
+//on click add the crystal amount to the users starting score of 0
+
+//loop if user gets exact number of score = win++, else if user goes OVER the generated number user loses++
