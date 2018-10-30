96a1cafac155b8998fb484c95b9533171fd627aa
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-10-30 18:20:40 -0500

    added basic html layout and inital pseudo code after reading hw instructions

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//row w/ heading & game name
+//row w/ 1 column listing directions/game objectives
+//row 2 colums
+    //one column holds random score generated
+    //another column holds win/loss record
+//row that contains clickable pictures of crystals  
+    //each crystal has a random number assigned at page load
+    //on click, amount of crystal is added to our score
+//row that lists our current score
+
