0d401b8a026eb1ec8ada9d5bde760f8a3ead3d6b
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-10-30 19:35:49 -0500

    diff ran number created for each crystal var now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,10 +17,14 @@ var losses = 0;
 var compRandomNum = Math.floor(Math.random() * 102) + 19;
 console.log(compRandomNum);
 //crystal random num
-var crystalValue = Math.floor(Math.random() * 12) + 1;
-console.log(crystalValue)
-
-//crystals generate their random numbers
+var crystal1 = Math.floor(Math.random() * 12) + 1;
+var crystal2 = Math.floor(Math.random() * 12) + 1;
+var crystal3 = Math.floor(Math.random() * 12) + 1;
+var crystal4 = Math.floor(Math.random() * 12) + 1;
+console.log("crystal1 " + crystal1);
+console.log("crystal2 " + crystal2);
+console.log("crystal3 " + crystal3);
+console.log("crystal4 " + crystal4);
 
 //hide crystal amount from user
 
